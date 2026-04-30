3678335a210966bcfe9af937e3ace1836cc1bee3
Hidd3n-47 <122316898+Hidd3n-47@users.noreply.github.com>
2026-04-30 15:23:56 +0100

    SBP-2: Simple character controller Added a very simple character controller that uses old input system just to have an object that moves around. Added: - Player prefab that can be dragged into a scene - Uses WASD to move - Can only turn when moving forward or backwards - Can change the rotation speed, normal and reversing speed from inspector Note: Scale is probably way off, just waiting on the first pass of forklift block-out then will update the prefab. No controller support at the moment Getting the gif ready or could have a quick discord call to show, whichever is easier

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float rotationRate = 20.0f;
+    [SerializeField] private float speed        = 20.0f;
+    [SerializeField] private float reverseSpeed = 15.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = 0.0f;
+        float turn = 0.0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move += 1.0f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move += -1.0f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            turn += -1.0f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            turn += 1.0f;
+        }
+
+        // If we are going backwards, flip the direction
+        if (move < 0.0f)
+        {
+            turn *= -1;
+        }
+
+        Transform t = GetComponent<Transform>();
+
+        t.eulerAngles += new Vector3(0.0f, turn * rotationRate * Time.deltaTime, 0.0f) * (!move.Equals(0.0f) ? 1.0f : 0.0f);
+        var delta = t.forward * move * (move < 0.0f ? reverseSpeed : speed);
+
+        t.position += delta * Time.deltaTime;
+    }
+}
