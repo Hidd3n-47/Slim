d577a9bd9f02a74a97ca3d46c4e2a1ad55cbc162
Hazar13 <hw3crt@bolton.ac.uk>
2026-05-13 18:00:46 +0100

    BottomWalls

--- Assets/Scripts/RandomWoodMaterial.cs
+++ Assets/Scripts/RandomWoodMaterial.cs
@@ -3,12 +3,7 @@ using UnityEngine;
 public class RandomWoodMaterial : MonoBehaviour
 {
     public Material mat;
-    public float hueMin = -1.0f;
-    public float hueMax = 1.0f;
-    public float saturationMin = -1.0f;
-    public float saturationMax = 1.0f;
-    public float brightnessMin = -1.0f;
-    public float brightnessMax = 1.0f;
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +14,9 @@ public class RandomWoodMaterial : MonoBehaviour
 
         var m = GetComponentInChildren<MeshRenderer>().material;
         var p = m.GetPropertyNames(MaterialPropertyType.Float);
-        m.SetFloat("_Hue", Random.Range(hueMin, hueMax));
-        m.SetFloat("_Saturation", Random.Range(saturationMin, saturationMax));
-        m.SetFloat("_Brightness", Random.Range(brightnessMin, brightnessMax));
+        m.SetFloat("_Hue", Random.Range(-15, 15));
+        m.SetFloat("_Saturation", Random.Range(0.6, 1.2));
+        m.SetFloat("_Brightness", Random.Range(-0.05, 0.05));
     }
 
     // Update is called once per frame
