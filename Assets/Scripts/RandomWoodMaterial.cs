3d240fca1da59afccbf63424e69396255a9e545b
alex <alexpercy00@live.co.uk>
2026-05-13 22:25:49 +0100

    Updated Player_V3 Prefab

--- Assets/Scripts/RandomWoodMaterial.cs
+++ Assets/Scripts/RandomWoodMaterial.cs
@@ -15,8 +15,8 @@ public class RandomWoodMaterial : MonoBehaviour
         var m = GetComponentInChildren<MeshRenderer>().material;
         var p = m.GetPropertyNames(MaterialPropertyType.Float);
         m.SetFloat("_Hue", Random.Range(-15, 15));
-        m.SetFloat("_Saturation", Random.Range(0.6, 1.2));
-        m.SetFloat("_Brightness", Random.Range(-0.05, 0.05));
+        m.SetFloat("_Saturation", Random.Range(0.6f, 1.2f));
+        m.SetFloat("_Brightness", Random.Range(-0.05f, 0.05f));
     }
 
     // Update is called once per frame
